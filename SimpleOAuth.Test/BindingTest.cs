5a19852c6c0552441adc8859c1f2acb238814560
ciptoj <ciptojohn@gmail.com>
2014-09-24 13:25:41 +0700

    Add Nunit and STA helper

---
+++ SimpleOAuth.Test/BindingTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AvalonUnitTesting;
+using SimpleOAuth.UI;
+using System.Windows;
+namespace SimpleOAuth.Test
+{
+    [TestClass]
+    public class BindingTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AvalonTestRunner.RunInSTA(
+           delegate
+           {
+               //MainWindow window = new MainWindow();
+               //Assert.AreEqual(480, ((System.Windows.Window)window).Height);
+               AvalonTestRunner.RunDataBindingTests(new MainWindow());               
+           });
+        }
+    }
+}
